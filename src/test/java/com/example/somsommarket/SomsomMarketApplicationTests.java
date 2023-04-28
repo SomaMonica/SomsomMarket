00113050e61175ced985a2164b40aa730e75c934
Juhee <viimv2000@gmail.com>
2023-04-28 14:56:44 +0900

    Initial commit

---
+++ src/test/java/com/example/somsommarket/SomsomMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.somsommarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomsomMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
